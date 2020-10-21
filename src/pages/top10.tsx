91ec16650ff84a71b87ee1eef0067cd1a9301160
Thiago Mendes <thiagoromendes@gmail.com>
2020-10-21 20:20:24 -0300

    Exemplo de static side generation com rotas dinâmicas

--- src/pages/top10.tsx
+++ src/pages/top10.tsx
@@ -26,7 +26,7 @@ export default function Top10({products}: Top10Props) {
     )
 }
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps<Top10Props> = async (context) => {
     const response = await fetch('http://localhost:3333/products')
     const products = await response.json()
 
